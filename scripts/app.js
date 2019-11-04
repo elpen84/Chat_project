e695282346ff7bdb91ff05fbd01945a5e9134413
elpen84 <elplist@gmail.com>
2019-11-04 00:30:12 -0800

    Initial commit

---
+++ scripts/app.js
@@ -0,0 +1,50 @@
+// dom queries
+const chatList = document.querySelector(".chat-list");
+const newChatForm = document.querySelector(".new-chat");
+const newNameForm = document.querySelector(".new-name");
+const updateMsg = document.querySelector(".update-mssg");
+const rooms = document.querySelector(".chat-rooms");
+
+// add new chat
+newChatForm.addEventListener("submit", e => {
+  e.preventDefault();
+  const message = newChatForm.message.value.trim();
+  chatroom
+    .addChat(message)
+    .then(() => newChatForm.reset())
+    .catch(err => console.log(err));
+});
+
+// check local storage for name
+
+const username = localStorage.username ? localStorage.username : "anon";
+
+// Class instances
+const chatUI = new ChatUI(chatList);
+const chatroom = new Chatroom("general", username);
+// get chats and render
+chatroom.getChats(data => {
+  chatUI.render(data);
+});
+
+//update username
+newNameForm.addEventListener("submit", e => {
+  e.preventDefault();
+  // update name via chatroom
+  const newName = newNameForm.name.value.trim();
+  chatroom.updateName(newName);
+  // reset form
+  newNameForm.reset();
+  // show and hide the update msg
+  updateMsg.innerText = `Your name was update to ${newName}`;
+  setTimeout(() => (updateMsg.innerText = ""), 3000);
+});
+
+// update the chat room
+rooms.addEventListener("click", e => {
+  if (e.target.tagName === "BUTTON") {
+    chatUI.clear();
+    chatroom.updateRoom(e.target.getAttribute("id"));
+    chatroom.getChats(chat => chatUI.render(chat));
+  }
+});
